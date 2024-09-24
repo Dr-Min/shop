daaa96b313aed5098342bf2dbc109fa4118a6348
min <mks010103@gamil.com>
2024-09-24 16:24:19 +0900

    블로그 숙제까지 완료 컴포넌트 map props 사용

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import { Button, Container, Nav, Navbar } from "react-bootstrap";
+import data from "./data.js";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  let [shoes] = useState(data);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">ComeOnMan</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <div className="main-bg"></div>
+      <Pages shoes={shoes}></Pages>
+    </div>
+  );
+}
+
+function Pages(props) {
+  return (
+    <div className="container">
+      <div className="row">
+        {props.shoes.map(function (shoe, i) {
+          return (
+            <div className="col-md-4" key={i}>
+              <img
+                src={`https://codingapple1.github.io/shop/shoes${i + 1}.jpg`}
+                width="80%"
+                alt={shoe.title}
+              />
+              <h4>{shoe.title}</h4>
+              <p>{shoe.price}</p>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
